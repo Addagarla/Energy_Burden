0e64ec517e1711ff8cda7ab30e9ffebb24dbf3b9
Addagarla <79801154+Addagarla@users.noreply.github.com>
2024-02-17 16:22:36 +0530

    first-commit
    integrated charts

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { DrillDownComponent } from "../drill-down/drill-down.component";
+import { BarComponent } from "../bar/bar.component";
+import { PieComponent } from "../pie/pie.component";
+import { PieDrillComponent } from "../pie-drill/pie-drill.component";
+import { StocksComponent } from "../stocks/stocks.component";
+import { GasBillComponent } from "../gas-bill/gas-bill.component";
+import { WaterBillComponent } from "../water-bill/water-bill.component";
+
+
+@Component({
+    selector: 'app-main-content',
+    standalone: true,
+    templateUrl: './main-content.component.html',
+    styleUrl: './main-content.component.css',
+    imports: [MatCardModule, DrillDownComponent, BarComponent, PieComponent, PieDrillComponent, StocksComponent, GasBillComponent, WaterBillComponent]
+})
+export class MainContentComponent {
+
+}
